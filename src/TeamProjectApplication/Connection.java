1e25215b6a7db97fc83a0fb10c549dec5e6ce3bc
the-w <the-w@DESKTOP-UTN7GRA>
2017-02-23 23:28:14 +0000

    Updated with active connection, needs test in college.

--- src/TeamProjectApplication/Connection.java
+++ src/TeamProjectApplication/Connection.java
@@ -11,13 +11,17 @@ public class Connection {
 		
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-			java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://local:3306/GardaDB","root","password");
+			java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/garda?autoReconnect=true&useSSL=false","root","Password");
 			st = con.createStatement();
-			rs = st.executeQuery("select * from case");
+			rs = st.executeQuery("select * from activecase");
 			
 			while(rs.next()){
-				System.out.println("\nID: " + rs.getInt(1));
-				System.out.println("\tStatus: " + rs.getString(6));
+				System.out.print("ID: " + rs.getInt(1));
+				System.out.print(" CrimeCode: " + rs.getInt(2));
+				System.out.print(" Date and Time: " + rs.getString(3));
+				System.out.print(" Location: " + rs.getString(4));
+				System.out.print(" Status: " + rs.getString(5));
+				System.out.print(" Eircode: " + rs.getString(6));
 			}
 		}
 		catch(Exception e){
